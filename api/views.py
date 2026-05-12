1192d8e4327ba7209afe49ac4daf333c25098e7c
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-05-11 22:18:44 -0300

    Adicionando Students dinamicamente

--- api/views.py
+++ api/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student # Para puxar dados dinamicamente
 
 def studentsView(request):
-    students = {
-        'id': 1,
-        'name': 'Andre',
-        'class': 'Computer Science'
-    }
-    return JsonResponse(students)
+    # students = {
+    #     'id': 1,
+    #     'name': 'Andre',
+    #     'class': 'Computer Science'
+    # }
+
+    # Adicionar estudantes dinamicamente
+    students = Student.objects.all()   
+    print(students) 
+    # Serializar para converter dados da consulta em uma lista (Não é padrão Rest)
+    students_list = list(students.values())
+    return JsonResponse(students_list, safe=False) # safe=False para que possamos mandar um query como retorno, e não um Json como esperado
--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Student # Adicionado para o model Student criado em students/models.py 
 
-# Register your models here.
+admin.site.register(Student) # Reguistrar Student no admin do Django
