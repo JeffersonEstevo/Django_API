bc47515704c3efa81ee5480eee7b3f963e904fbf
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-05-15 22:34:30 -0300

    Class-Based View - Criando um funcionários (Employee)

--- api/views.py
+++ api/views.py
@@ -131,3 +131,10 @@ class Employees(APIView):
         # Retorna uma resposta HTTP contendo os dados formatados em JSON e o código de status HTTP 200 (OK).
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
